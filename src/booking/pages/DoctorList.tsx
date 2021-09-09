7af46a23428d9c945b9a553cac356122ce50805a
dangvoquanglieu <55011549+dangvoquanglieu@users.noreply.github.com>
2021-09-09 16:17:27 +0700

    update inifiteScroll doctorList UI

--- src/booking/pages/DoctorList.tsx
+++ src/booking/pages/DoctorList.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   IonCard,
-  IonCardContent,
   IonContent,
   IonHeader,
   IonIcon,
@@ -15,13 +14,10 @@ import {
   IonSpinner,
   IonInfiniteScrollContent,
   IonInfiniteScroll,
-  useIonViewWillEnter
 } from "@ionic/react";
 import { useDispatch, useSelector } from "@app/hooks";
 import { useHistory } from "react-router-dom";
 import {
-  search,
-  chatbubbles,
   chevronBack,
   peopleCircleSharp,
   filter,
@@ -29,8 +25,7 @@ import {
 import { useTranslation } from "react-i18next";
 import styles from "../css/doctorList.module.css";
 import classNames from 'classnames/bind';
-import { deburr } from '../../@app/utils/helpers';
-import { Doctor, DoctorData } from "booking/models/hospital";
+import { DoctorData } from "booking/models/hospital";
 import { getAllDoctor } from "booking/slices/hospital";
 
 
@@ -39,130 +34,94 @@ const DoctorList: React.FC = () => {
   const [searchInput, setSearchInput] = useState(false);
   const { t } = useTranslation();
   const history = useHistory();
-  const { loading, doctorList } = useSelector((b) => b.hospital);
-  const [listRender, setListRender] = useState<DoctorData[]>([]);
-  const [disableInfiniteScroll, setDisableInfiniteScroll] = useState<boolean>(false);
-  const [pageIndex, setPageIndex] = useState<number>(1);
+  const { loading, doctorList, } = useSelector((b) => b.hospital);
+
+  const [pageSize, setPageSize] = useState<number>(5);
   const [nameSearch, setNameSearch] = useState("")
   var cx = classNames.bind(styles);
   const doctorListSuccess = doctorList?.data;
-  const doctorListSearch = doctorListSuccess?.filter(d => deburr(d.fullName +
-    d.academicTitle +
-    d.email + d.gender +
-    d.phone +
-    d.title
-  ).includes(deburr(nameSearch)));
 
-  const getData = async () => {
-    await dispatch(getAllDoctor({ pageIndex: pageIndex, pageSize: 5 }));
-    // dispatch(setListDoctorRender(doctorList.data))
-    Boolean(doctorList.data.length > 0) ?
-      setDisableInfiniteScroll(false)
-      : setDisableInfiniteScroll(true);
-     setListRender([...listRender, ...doctorList.data]);
-    setPageIndex(pageIndex + 1);
+  const getData = useCallback(() => {
+    dispatch(getAllDoctor({ pageIndex: 0, pageSize: pageSize }));
+  }, [pageSize]);
+
+  const fetchData = () => {
+    pageSize <= doctorList.totalPage * 5 ? 
+    setPageSize(pageSize + 5) : console.log()
   }
 
-  const handleInfiniteScroll = async ($event: CustomEvent<void>) => {
-    await getData();
+  const handleInfiniteScroll = ($event: CustomEvent<void>) => {
+    fetchData();
     ($event.target as HTMLIonInfiniteScrollElement).complete();
-
   }
 
-  useEffect(() => { getData() },[]);
+  useEffect(getData, [getData]);
 
   return (
-    loading === true ? <IonSpinner name='bubbles' color='primary' style={{ left: '50%', top: '50%' }}></IonSpinner> :
-      <IonPage className={styles.styledPage}>
-        {/* {searchInput === false ? */}
-        <IonHeader className={styles.header}>
-          <IonIcon
-            onClick={() => history.goBack()}
-            className={styles.iconLeft}
-            icon={chevronBack}></IonIcon>
-          <IonLabel className={styles.headerLabel}>{t('List of doctors')} </IonLabel>
-          {/* <IonIcon
-              onClick={() => setSearchInput(true)}
-              className={styles.iconRight}
-              icon={search}></IonIcon> */}
-        </IonHeader>
-        {/* : <div className={styles.headerSearch}>
-            <IonSearchbar onIonChange={(e) => setNameSearch(e.detail.value!)} placeholder={t('Search')} className={styles.searchBar}></IonSearchbar>
-            <button onClick={() => setSearchInput(false)} className={styles.cancelSearch}>Cancel</button>
-          </div>} */}
-        <IonContent className={styles.content}>
+    <IonPage className={styles.styledPage}>
+      <IonHeader className={styles.header}>
+        <IonIcon
+          onClick={() => history.goBack()}
+          className={styles.iconLeft}
+          icon={chevronBack}></IonIcon>
+        <IonLabel className={styles.headerLabel}>{t('List of doctors')} </IonLabel>
+      </IonHeader>
+      
+      <IonContent className={styles.content}>
 
-          <div className={styles.headerSearch}>
-            <IonSearchbar
-              onIonChange={(e) => setNameSearch(e.detail.value!)}
-              placeholder={t('Nhập thông tin CBO')}
-              className={styles.searchBar}></IonSearchbar>
-            <button onClick={() => setSearchInput(false)} className={styles.btnFilter}>
-              <IonIcon className={styles.iconFilter} icon={filter}></IonIcon>
-            </button>
-          </div>
+        <div className={styles.headerSearch}>
+          <IonSearchbar
+            onIonChange={(e) => setNameSearch(e.detail.value!)}
+            placeholder={t('Enter CBO Information')}
+            className={styles.searchBar}></IonSearchbar>
+          <button onClick={() => setSearchInput(false)} className={styles.btnFilter}>
+            <IonIcon className={styles.iconFilter} icon={filter}></IonIcon>
+          </button>
+        </div>
 
-          {/* {searchInput === true ? */}
-          <>
-            {listRender.map((d) => {
-              return (
-                <div className={styles.styledCardDiv}>
-                  <IonCard
-                    onClick={() => history.push({ pathname: '/doctorDetail', state: d as DoctorData })}
-                    className={cx('styledCard', {})}
-                  >
-                    <div
-                      className={cx('styledDivIcon',)}
-                    >
-                      <IonIcon className={styles.styledIcon} icon={peopleCircleSharp}></IonIcon>
-                    </div>
-                    <IonCardHeader
-                      className={cx('styledCardHeader')}
-                    >
-                      <IonCardTitle
-                        className={cx('styledCardTitle',)}
-                      >{d?.fullName}
-                      </IonCardTitle>
-                      <IonCardSubtitle
-                        className={cx('styledSubtitle')}
-                      >{d?.phone}</IonCardSubtitle>
-                      <IonCardSubtitle
-                        className={cx('styledSubtitle')}
-                      >{d?.email}</IonCardSubtitle>
-                    </IonCardHeader>
-                    {/* <IonCardContent
-                      className={cx('styledCardContent')}
-                    >
-                      <p>{d?.email}</p>
-                      <p>{d?.phone}</p>
-                    </IonCardContent> */}
-
-                  </IonCard>
-                  {/* <button onClick={() => {
-                    history.push({ pathname: '/doctorDetail', state: d as DoctorData })
-                  }} className={styles.btnCancelCard}>{t('Cancel appointment')}</button> */}
-
+        <>
+          {doctorListSuccess.map((d) =>
+            <div className={styles.styledCardDiv}>
+              <IonCard
+                onClick={() => history.push({ pathname: '/doctorDetail', state: d as DoctorData })}
+                className={cx('styledCard', {})}
+              >
+                <div
+                  className={cx('styledDivIcon',)}
+                >
+                  <IonIcon className={styles.styledIcon} icon={peopleCircleSharp}></IonIcon>
                 </div>
-              )
-            }
-            )}
-            <IonInfiniteScroll
-            threshold="100px"
-              onIonInfinite={(e: CustomEvent<void>) => handleInfiniteScroll(e)}
-              disabled={false}
-            >
-              <IonInfiniteScrollContent loadingText="Loading more good doctor...">
-
+                <IonCardHeader
+                  className={cx('styledCardHeader')}
+                >
+                  <IonCardTitle
+                    className={cx('styledCardTitle',)}
+                  >{d?.fullName}
+                  </IonCardTitle>
+                  <IonCardSubtitle
+                    className={cx('styledSubtitle')}
+                  >{d?.phone}</IonCardSubtitle>
+                  <IonCardSubtitle
+                    className={cx('styledSubtitle')}
+                  >{d?.email}</IonCardSubtitle>
+                </IonCardHeader>
+              </IonCard>
+            </div>
+          )}
+          <div>
+            <IonInfiniteScroll threshold="100px"
+              onIonInfinite={(e: CustomEvent<void>) => handleInfiniteScroll(e)}>
+              <IonInfiniteScrollContent
+                loadingSpinner="bubbles"
+                loadingText="Loading more data..."
+              >
+                {loading ? <IonSpinner name='bubbles' color='primary' style={{ left: '50%' }}></IonSpinner> : null}
               </IonInfiniteScrollContent>
             </IonInfiniteScroll>
-            {/* <IonLabel className={styles.styledLabel}>{t("Danh sách bác sĩ")}</IonLabel> */}
-
-          </>
-
-
-
-        </IonContent>
-      </IonPage>
+          </div>
+        </>
+      </IonContent>
+    </IonPage>
   );
 };
 

bf3681240331d590e4df95bebaf2bad1a5e740e8
ndkai <darkspirit909@gmail.com>
2022-01-04 14:43:53 +0700

    Add trustkit for android

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,7 +1,16 @@
 package io.ionic.starter;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.datatheorem.android.trustkit.TrustKit;
+import com.datatheorem.android.trustkit.reporting.BackgroundReporter;
+import com.datatheorem.android.trustkit.reporting.PinningFailureReport;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
@@ -10,10 +19,20 @@ import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 import com.whitestein.securestorage.SecureStoragePlugin;
 
 public class MainActivity extends BridgeActivity {
+  PinningFailureReportBroadcastReceiver pinningFailureReportBroadcastReceiver;
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     // Initializes the Bridge
+    if (android.os.Build.VERSION.SDK_INT <= 24){
+      // Do something for lollipop and above versions
+      TrustKit.initializeWithNetworkSecurityConfiguration(this);
+      pinningFailureReportBroadcastReceiver
+              = new PinningFailureReportBroadcastReceiver();
+      IntentFilter intentFilter = new IntentFilter(BackgroundReporter.REPORT_VALIDATION_EVENT)  ;
+      LocalBroadcastManager.getInstance(this)
+              .registerReceiver(pinningFailureReportBroadcastReceiver,intentFilter) ;
+    }
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
@@ -21,6 +40,14 @@ public class MainActivity extends BridgeActivity {
       add(com.getcapacitor.community.facebooklogin.FacebookLogin.class);
       add(SecureStoragePlugin.class);
     }});
-    
+
+  }
+}
+
+class PinningFailureReportBroadcastReceiver extends BroadcastReceiver {
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    PinningFailureReport report = (PinningFailureReport) intent.getSerializableExtra(BackgroundReporter.EXTRA_REPORT);
   }
 }
